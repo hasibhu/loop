95b215032b493c5096b2ac4695ff505c1bcde206
Hasibul Islam <hasibulislam@Hasibs-Mac-Studio-M2-Max.local>
2024-01-29 15:25:54 +0100

    even and range files addede

---
+++ even.js
@@ -0,0 +1,15 @@
+
+
+let num = 1;
+
+while(num <= 23){
+    console.log(num);
+
+    if(num%2 === 0){
+        console.log(`The number ${num} is an even number.`);
+    } else{
+        console.log(`The number ${num} is an odd number.`);
+    }
+
+    num++;
+}
--- whileLoop.js
+++ whileLoop.js
@@ -1,5 +1,5 @@
 
-
+// with numbers
 let numbers = [12, 98, 45, 72, 22, 66, 89]
 
 let i = 0; 
@@ -7,3 +7,15 @@ while(i < numbers.length){
     console.log(numbers[i]);
     i++;
 }
+
+// with names
+
+console.log(`Now I will play with strings`);
+let names = ["Berlin", "Moscow", "London", "Dhaka", "Copenhagen"]
+
+let x = 0; 
+while(x < names.length){
+    console.log(names[x]);
+    x++;
+}
+
