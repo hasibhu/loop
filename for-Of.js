2f14470c63b3ccfb8794d125c3c1bb3f4969c048
Hasibul Islam <hasibulislam@Hasibs-Mac-Studio-M2-Max.local>
2024-01-29 14:51:51 +0100

    new file added

---
+++ for-Of.js
@@ -0,0 +1,15 @@
+
+// with string
+let fruits = ["Mango", "Oragne", "Apple", "Banana", "Blackberry"];
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+// with numbers
+let numbers = [12, 67, 43, 99, 23, 67, 51];
+let i = 0;
+for(let num of numbers){
+    console.log(`The index of number ${num} is ${i} `);
+    i++
+}
---
+++ whileLoop.js
@@ -0,0 +1,9 @@
+
+
+let numbers = [12, 98, 45, 72, 22, 66, 89]
+
+let i = 0; 
+while(i < numbers.length){
+    console.log(numbers[i]);
+    i++;
+}
